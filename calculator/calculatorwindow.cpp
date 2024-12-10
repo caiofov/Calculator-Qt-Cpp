f8d46ab90d74fe09d749ef26a01e546ad27a7054
Caio Oliveira <cfoviana@alu.ufc.br>
2024-12-10 12:13:17 -0300

    feat: project structure

---
+++ calculator/calculatorwindow.cpp
@@ -0,0 +1,14 @@
+#include "calculatorwindow.h"
+#include "./ui_calculatorwindow.h"
+
+CalculatorWindow::CalculatorWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::CalculatorWindow)
+{
+    ui->setupUi(this);
+}
+
+CalculatorWindow::~CalculatorWindow()
+{
+    delete ui;
+}
